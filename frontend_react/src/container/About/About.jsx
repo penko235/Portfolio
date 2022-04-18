638aee1f9ee21f0d91778dc5befd7687833695f9
penko235 <penko032@gmail.com>
2022-04-18 21:12:03 +0300

    HOC & Work section

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -1,6 +1,8 @@
 import React, {useState, useEffect} from 'react';
 import { motion } from 'framer-motion';
 
+import { AppWrap } from '../../wrapper';
+
 import { images } from '../../constants';
 import './About.scss';
 import { urlFor, client } from '../../client';
@@ -44,4 +46,4 @@ const About = () => {
   )
 }
 
-export default About;
+export default AppWrap(About, 'about');
