b42ab2a28cf4740a06a3bf116fce8b3facd41a33
penko235 <penko032@gmail.com>
2022-04-26 23:01:20 +0300

    Fixes after testing on different devices

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -41,7 +41,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['DevBook', 'Salesforce', 'Cryptocurrency Tracker App', 'WordBeater', 'All'].map((item, index) => (
+        {['DevBook', 'Salesforce', 'Crypto Tracker App', 'WordBeater', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
